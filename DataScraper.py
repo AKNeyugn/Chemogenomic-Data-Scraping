b521160699ec716992e953680a826edb7766fa25
AKNeyugn <anhkhoi.nguyenhoang@gmail.com>
2019-06-04 17:23:02 -0500

    Implemented web scraper and CGM creator

--- DataScraper.py
+++ DataScraper.py
@@ -1,49 +1,133 @@
-"""
+#!/usr/bin/env python
+
+""" For each compound library available on http://chemgrid.org/cgm/index.php,
+    download all compounds vs mutant .csv data files
+
+    Author: Roy Nguyen
+    Last edited: June 4, 2019
 """
 
 import sys
 import os
+import datetime
 from bs4 import BeautifulSoup
 import requests
 import csv
+import pandas as pd
 
 
 main_url_start = "http://chemgrid.org/cgm/screens.php?sublib="
 main_url_end = "&submit=Choose+DB"
 data_url_parts = ["http://chemgrid.org/cgm/do_plot.php?p=1&pID=1&l=", "&sp=",
                 "&pn=0&r=", "&cid=0&s=", "&out=csv&fn=./uploads/plot_", "_", "_",
-                "__pn0"]
+                "__pn0"]    # constant parts of data file download link
 library_indices = [1,2,3,4,5,7,8]    # url index of each CGM library
+data_output_folder = "Data-Files/"
+cgm_output_folder = "CGM/"
 
 def main():
-    scraper()
+    start = datetime.datetime.now()
+    sys.stdout.write("Start time: " + str(start) + "\n")
+    sys.stdout.write("\n")
 
-    sys.stdout.write("Done!")
+    #scraper()
+    create_cgm("Data-Files/Spectrum_ED")
+
+    end = datetime.datetime.now()
+    time_taken = end - start
+    sys.stdout.write("\n")
+    sys.stdout.write("Time taken: " + str(time_taken.seconds // 60) + " minutes " 
+                    + str(time_taken.seconds % 60) + " seconds. \n")
+    sys.stdout.write("Script finished! \n")
     return
 
 def scraper():
     '''
+    For each compound library, download all mutant .csv data files
     '''
     for i in library_indices:
+        folder_name = data_output_folder
         main_url = main_url_start + str(i) + main_url_end
         main_response = requests.get(main_url)
-        folder_name = ""
-        if i == 1:
-            main_page_content = BeautifulSoup(main_response.content, "html.parser")
-            table = main_page_content.find_all("table")[2]
-            folder_name = table.find_all("tr")[1].find_all("td")[1].get_text()
-            strain_list = table.find_all("tr")
-            strain_range = len(strain_list)
-            sys.stdout.write("Parsing " + folder_name + " library... \n")
-            for j in range(strain_range):
-                if j != 0:
-                    strain_info = strain_list[j].find_all("td")
-                    library_name = strain_info[1].get_text()
-                    strain_name = strain_info[0].get_text()
+        main_page_content = BeautifulSoup(main_response.content, "html.parser")
+        table = main_page_content.find_all("table")[2]
+
+        # Create output folder for library
+        library_name = table.find_all("tr")[1].find_all("td")[1].get_text()
+        folder_name += library_name
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+        strain_list = table.find_all("tr")
+        strain_range = len(strain_list)
+        num_mutants_processed = 0
+        sys.stdout.write("Parsing " + library_name + " library... \n")
+        for j in range(strain_range):
+            if j != 0:
+                strain_info = strain_list[j].find_all("td")
+                strain_name = strain_info[0].get_text()
+                # Filter out SPE0xxxx (compound) strains in Maybridge library
+                if "SPE0" not in strain_name:
                     num_species = strain_info[2].get_text()
                     num_plates = strain_info[3].get_text()
+                    num_bioactive = strain_info[6].get_text()
+                    num_toxic = strain_info[7].get_text()
                     data_url = data_url_former(library_name, strain_name, num_species, num_plates)
+
+                    # Write data into local .csv file
+                    data = requests.get(data_url)
+                    file_name = strain_name + "_" + num_bioactive + "_" + num_toxic
+                    output_name = folder_name + "/" + file_name + ".csv"
+                    with open(output_name, "w", newline="") as f:
+                        writer = csv.writer(f)
+                        reader = csv.reader(data.text.splitlines())
+
+                        for row in reader:
+                            writer.writerow(row)
+                    num_mutants_processed += 1
+                
+            sys.stdout.write("Processed %d mutants \r" % (j))
+            sys.stdout.flush()
+        
+        sys.stdout.write("Downloaded %d mutant data files! \n" % (num_mutants_processed))
+
     return
+
+def create_cgm(folder_name):
+    '''
+    Create a chemical genomic matrix (compounds vs mutants) from data files
+    in folder_name and output into a Excel file
+
+    Args:
+        folder_name (string): name of folder containing mutant data files
+    '''
+    # Create output folder if not exists
+    if not os.path.exists(cgm_output_folder):
+        os.makedirs(cgm_output_folder)
+
+    library_name = folder_name[folder_name.index("/")+1:]
+    sys.stdout.write("Creating CGM for " + library_name + "... \n")
+    list_data = os.listdir(folder_name)
+
+    # Parse first data file to initialize output DataFrame
+    file_name = folder_name + "/" + list_data[0]
+    output_name = cgm_output_folder + library_name + "_CGM.csv"
+    df = pd.read_csv(file_name)
+    indices = list(df[df.columns[0]])
+    mutant_name = list_data[0][:list_data[0].index(".csv")]
+    output = pd.DataFrame()
+    
+    for i in range(len(list_data)):
+        file_name = folder_name + "/" + list_data[i]
+        df = pd.read_csv(file_name)
+        indices = df[df.columns[0]]
+        mutant_name = list_data[i][:list_data[i].index(".csv")]
+        tmp_output = pd.DataFrame(list(df[df.columns[2]]), columns=[mutant_name], index=indices)
+        output = pd.concat([output, tmp_output], axis=1, sort=False)
+
+    output.to_csv(output_name)
+    return
+
 
 def data_url_former(library_name, strain_name, num_species, num_plates):
     '''
@@ -70,22 +154,20 @@ def data_url_former(library_name, strain_name, num_species, num_plates):
 
     return data_url
 
+def extract_name(strain_info):
+    '''
+    Get strain name
+
+    Args:
+        strain_info (string): url containing strain name
+
+    Return:
+        (string): strain name
+    '''
+    start_index = strain_info.index("id=") + 3
+    strain_name = strain_info[start_index:]
+
+    return strain_name
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-'''
-url = "http://chemgrid.org/cgm/do_plot.php?p=1&pID=1&l=Lopac&sp=4932&pn=0&r=16&cid=0&s=MT2481-pdr1pdr3&out=csv&fn=./uploads/plot_4932_Lopac_MT2481-pdr1pdr3__pn0"
-
-data = requests.get(url)
-with open("out.csv", "w") as f:
-    writer = csv.writer(f)
-    reader = csv.reader(data.text.splitlines())
-
-    for row in reader:
-        writer.writerow(row)
-        '''
