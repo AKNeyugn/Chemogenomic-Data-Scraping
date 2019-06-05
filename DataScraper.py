8117e9ca3ea2b5cbbdc6629d227842c97e86937e
AKNeyugn <anhkhoi.nguyenhoang@gmail.com>
2019-06-05 12:38:29 -0500

    implement change to run on Linux

--- DataScraper.py
+++ DataScraper.py
@@ -23,19 +23,22 @@ data_url_parts = ["http://chemgrid.org/cgm/do_plot.php?p=1&pID=1&l=", "&sp=",
                 "&pn=0&r=", "&cid=0&s=", "&out=csv&fn=./uploads/plot_", "_", "_",
                 "__pn0"]    # constant parts of data file download link
 library_indices = [1,2,3,4,5,7,8]    # url index of each CGM library
-data_output_folder = "Data-Files/"
-cgm_output_folder = "CGM/"
+data_output_folder = "Data-Files"
+cgm_output_folder = "CGM"
 
 def main():
     start = datetime.datetime.now()
     sys.stdout.write("Start time: " + str(start) + "\n")
     sys.stdout.write("\n")
 
-    #scraper()
+    cwd = os.getcwd()
+
+    scraper()
     libraries = os.listdir(data_output_folder)
     for library in libraries:
-        dir_name = data_output_folder + library
-        create_cgm(dir_name)
+        tmp_dir_name = form_path(cwd, data_output_folder)
+        dir_name = form_path(tmp_dir_name, library)
+        create_cgm(dir_name, library)
 
     end = datetime.datetime.now()
     time_taken = end - start
@@ -49,8 +52,9 @@ def scraper():
     '''
     For each compound library, download all mutant .csv data files
     '''
+    cwd = os.getcwd()
     for i in library_indices:
-        folder_name = data_output_folder
+        folder_name = form_path(cwd, data_output_folder)
         main_url = main_url_start + str(i) + main_url_end
         main_response = requests.get(main_url)
         main_page_content = BeautifulSoup(main_response.content, "html.parser")
@@ -58,9 +62,9 @@ def scraper():
 
         # Create output folder for library
         library_name = table.find_all("tr")[1].find_all("td")[1].get_text()
-        folder_name += library_name
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
+        library_folder = form_path(folder_name, library_name)
+        if not os.path.exists(library_folder):
+            os.makedirs(library_folder)
 
         strain_list = table.find_all("tr")
         strain_range = len(strain_list)
@@ -69,9 +73,10 @@ def scraper():
         for j in range(strain_range):
             if j != 0:
                 strain_info = strain_list[j].find_all("td")
-                strain_name = strain_info[0].get_text()
+                tmp_strain_name = strain_info[0].get_text()
+                strain_name = extract_name(strain_info[6].find("a").get("href"))
                 # Filter out SPE0xxxx (compound) strains in Maybridge library
-                if "SPE0" not in strain_name:
+                if "SPE0" not in tmp_strain_name:
                     num_species = strain_info[2].get_text()
                     num_plates = strain_info[3].get_text()
                     num_bioactive = strain_info[6].get_text()
@@ -80,8 +85,8 @@ def scraper():
 
                     # Write data into local .csv file
                     data = requests.get(data_url)
-                    file_name = strain_name + "_" + num_bioactive + "_" + num_toxic
-                    output_name = folder_name + "/" + file_name + ".csv"
+                    file_name = strain_name + "_" + num_bioactive + "_" + num_toxic + ".csv"
+                    output_name = form_path(library_folder, file_name)
                     with open(output_name, "w", newline="") as f:
                         writer = csv.writer(f)
                         reader = csv.reader(data.text.splitlines())
@@ -97,26 +102,30 @@ def scraper():
 
     return
 
-def create_cgm(folder_name):
+def create_cgm(folder_name, library_name):
     '''
     Create a chemical genomic matrix (compounds vs mutants) from data files
     in folder_name and output into a Excel file
 
     Args:
         folder_name (string): name of folder containing mutant data files
+        library_name (string): name of compound library
     '''
     # Create output folder if not exists
-    if not os.path.exists(cgm_output_folder):
-        os.makedirs(cgm_output_folder)
+    cwd = os.getcwd()
+    output_folder = form_path(cwd, cgm_output_folder)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-    library_name = folder_name[folder_name.index("/")+1:]
-    sys.stdout.write("Creating CGM for " + library_name + "... \n")
+    library = library_name
+    sys.stdout.write("Creating CGM for " + library + "... \n")
     list_data = os.listdir(folder_name)
-    output_name = cgm_output_folder + library_name + "_CGM.csv"
+    output_file_name = library + "_CGM.csv"
+    output_name = form_path(output_folder, output_file_name)
     output = pd.DataFrame()
     
     for i in range(len(list_data)):
-        file_name = folder_name + "/" + list_data[i]
+        file_name = form_path(folder_name, list_data[i])
         df = pd.read_csv(file_name)
         indices = df[df.columns[0]]
         mutant_name = list_data[i][:list_data[i].index(".csv")]
@@ -151,6 +160,37 @@ def data_url_former(library_name, strain_name, num_species, num_plates):
     data_url += data_url_parts[7]
 
     return data_url
+
+def extract_name(strain_info):
+    '''
+    Get strain name
+    Args:
+        strain_info (string): url containing strain name
+    Return:
+        (string): strain name
+    '''
+    start_index = strain_info.index("id=") + 3
+    strain_name = strain_info[start_index:]
+    return strain_name
+
+def form_path(start, end):
+    '''
+    Create string representing a path depending on
+    Windows/Linux environment
+
+    Args:
+        start (string): start of output path
+        end (string): string to add to end of start
+
+    Return:
+        (string): full path combining start and end
+    '''
+    path = ""
+    if "/" in start:
+        path = start + "/" + end
+    elif "\\" in start:
+        path = start + "\\" + end
+    return path
 
 if __name__ == "__main__":
     main()
